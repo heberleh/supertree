630a50fa3edf6a5b603e6a1c48e9669f63f48d8b
Henry Heberle <henryhbrl@gmail.com>
2018-03-25 03:38:24 -0300

    Nav-bar working. Tree symbol added. Sugested name: sTVis - superTrees Visualization

--- web_supertree/accounts/admin.py
+++ web_supertree/accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Tree, Supertree
+from .models import Tree, Supertree, UserProfile
 
 # Register your models here.
 
+admin.site.register(UserProfile)
 admin.site.register(Supertree)
 admin.site.register(Tree)
