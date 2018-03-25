c470de7181687237e8c3cecb24e98199fd6ff08f
Henry Heberle <henryhbrl@gmail.com>
2018-03-25 15:25:54 -0300

    nav bar with login/logout/signin symbols and urls

--- web_supertree/accounts/urls.py
+++ web_supertree/accounts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+
+from django.contrib.auth.views import login, logout
 
 
 app_name = 'accounts'
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^login/$', login, {'template_name':'accounts/login.html'}),
+    url(r'^$', views.home, name="home"),
+    url(r'^login/$', login, {'template_name':'accounts/login.html'}, name="login"),
+    url(r'^logout/$', logout, {'template_name':'accounts/logout.html'}, name="logout"),
+    url(r'^signin/$', logout, {'template_name':'accounts/signin.html'}, name="signin"),
+    url(r'^profile/$', logout, {'template_name':'accounts/profile.html'}, name="profile"),
 
 
 #     path('', views.IndexView.as_view(template_name='app/index.html'), name='index'),
--- web_supertree/web_supertree/urls.py
+++ web_supertree/web_supertree/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
     path('account/', include('accounts.urls')),
     path('admin/', admin.site.urls),    
 ]
